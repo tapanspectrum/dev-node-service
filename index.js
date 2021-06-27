59ab77c2a8cb90dbae6e54fc28072ea241052954
tapanspectrum@gmail.com <tapanspectrum@gmail.com>
2021-06-27 15:11:35 +0530

    UserAuth and post code commited

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 //initalize dotenv
 const dotenv = require("dotenv").config();
+
 //mongodb configg
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }, () => {
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology:true }, () => {
   console.log("DB Connected");
 });
+
 //Middleware start here
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+//Define routes
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
+// Server initalize here
 app.listen(process.env.port, () => {
   console.log(`Backend server is running port number ${process.env.port}`);
 });
